8535de55f5d327195fdeadc85786c53e6e8edff6
Franco-Paredes <flpg2017@gmail.com>
2026-02-24 12:26:17 -0500

    Update Datos-simples.py

--- tipos de datos/Datos-simples.py
+++ tipos de datos/Datos-simples.py
@@ -1,2 +1,21 @@
-"5+5"
+#String-->cadenas de texto
+
+#"Strings"-->Cadena de texto simple, una sola línea de codigo, indica inicio y final en esa línea.
+#"""Strings"""-->Cadena de texto multilinea, se puede escribir en varias líneas, indica inicio y final en varias líneas.
+
+#Ejemplo de String
+name = "John Doe"
+print(name)
+
+#Ejemplo de String multilinea
+description = """John Doe is a software developer
+He loves coding and learning new technologies."""
+print(description)
+
+#Int-->Números enteros, sin decimales. -->40, -5, 0
+#Float-->Números con decimales. --->40.3, 0.0, 30.2
+
+#Valores Boleanos-->True o False, se utilizan para representar condiciones o estados.
+False_value = False
+True_value = True   
 
